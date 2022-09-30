4ed8686552c83fc967e4127e6be94eafffe9df16
John Doe <ramazonov3600@gmail.com>
2022-09-30 11:05:18 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".site-list").slick({
+        arrows:true,
+        dots:true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
