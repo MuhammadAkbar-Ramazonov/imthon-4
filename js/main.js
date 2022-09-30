5004a98eb9e47b06ddbc971b6b151f3de5aa53d1
John Doe <ramazonov3600@gmail.com>
2022-09-30 22:53:53 +0500

    add slick

--- js/main.js
+++ js/main.js
@@ -1,17 +1,40 @@
+var elOpenBtn = document.querySelector(".js-header-btn");
+var elSiteNav = document.querySelector(".site-nav");
+
+
+// elOpenBtn.addEventListener("click", function(){
+//     elSiteNav.classList.toggle("show-block");
+// });
+
+const hamburgerButton = document.querySelector(".burger")
+
+hamburgerButton.addEventListener("click", () => {
+    document.body.toggleAttribute("no-scroll")
+    if (elSiteNav.classList.contains("show")) {
+        elSiteNav.classList.add("closing");
+        elSiteNav.addEventListener("animationend", () => {
+            elSiteNav.classList.remove("show", "closing")
+        }, { once: true })
+        return
+    }
+    elSiteNav.classList.add("show")
+})
+
+console.log(123);
+
+
 $(document).ready(function(){
     $(".site-list").slick({
-        arrows:true,
-        dots:true,
+        arrows: true,
+        dots: false,
         slidesToShow: 3,
         slidesToScroll: 1,
         speed: 800,
         easing: 'easy',
-        // infinite: true,
         autoplay: true,
         autoplaySpeed: 3000,
         pauseOnHover: true,
         pauseOnFocus: true,
         pauseOnDotsHover: true,
-    
     });
 });
